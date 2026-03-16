99cfa34d5d5882c13c7602abfda63d0f39a3cf8a
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-03-16 18:34:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.27%) - LeetSync

---
+++ 202-happy-number/happy-number.c
@@ -0,0 +1,17 @@
+bool isHappy(int n) {
+
+     while(n>9){
+        int sum=0;
+        while(n!=0){
+            sum+=pow(n%10,2);
+            n/=10;
+        }
+        n=sum;
+     }
+     if(n==1 || n==7)
+     return true;
+     else
+     return false;
+
+    
+}
