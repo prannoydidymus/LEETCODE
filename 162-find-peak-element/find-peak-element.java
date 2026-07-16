56a32f544210b5653cbd00acc6e662dc2a52c800
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-07-16 18:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (56.09%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,20 +1,17 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-int n = nums.length;
-if(n == 0){
-    return -1;
+        int n = nums.length;
+        if(n==0){
+            return -1;
+        }
+        int c = nums[0];
+        int index = 0;
+        for(int i = 0;i<n;i++){
+            if (c < nums[i]){
+                c = nums[i];
+                index = i;
+            }
+        }    
+        return index;
+        }
 }
-    int max = nums[0];
-    int index = 0;
-
-for(int i =1;i<n;i++){
-if(nums[i] > max){
-    max = nums[i];
-    index = i;
-}
-
-}
-return index;
-    }
-}
-
