14f1a4c95792009b5d5d3b0c57067b6c80342604
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-07-16 18:26:50 +0530

    Time: 34 ms (90.69%) | Memory: 20.6 MB (95.56%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+
         nums.sort()
         return nums
         
