443f0f9c16e65ea45a0fc6f0e34d0c8bdfa7a757
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-07-22 18:23:08 +0530

    Time: 1 ms (99.89%) | Memory: 55.5 MB (66.51%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    int count = 0;
+    int major = 0;
+    for(int num: nums){
+        if(count == 0){
+            major = num;
+        }
+        if(num == major){
+            count++;
+        }
+        else{
+            count--;
+        }
+
+    }
+     return major;
+    }       
+}
