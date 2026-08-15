4a88b257ab86c7085e1368e500127c54e741cc19
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-08-15 18:24:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (51.14%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,12 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-            int n = strs.length;
-        if(strs == null || n == 0){
-            return "";
-        }
-    String a = strs[0];
-        for(int i = 1;i<n;i++){
-            while(strs[i].indexOf(a) != 0){
-        a = a.substring(0,a.length() - 1);
-            if(a.isEmpty()){
-                return "";
+        if(strs.length == 0) return "";
+        String a = strs[0];
+        for(int i = 1;i<strs.length;i++){
+            while(strs[i].indexOf(a)!=0){
+                a = a.substring(0,a.length() - 1);
             }
         }
-    }
         return a;
     }
 }
