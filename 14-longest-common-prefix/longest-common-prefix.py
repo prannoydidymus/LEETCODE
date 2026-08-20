7bdf169f54196351e825c0d5dac898240985e330
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-08-20 08:49:42 +0530

    Time: 1 ms (26.89%) | Memory: 19.3 MB (31.53%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,9 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if(len(strs) == 0):
-             return ""
+            return ""
         a = strs[0]
         for i in range(1,len(strs)):
             while not strs[i].startswith(a):
-                a = a[:-1]
+                a  = a[:-1]
         return a
+
+        
