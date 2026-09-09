084755ad5834d91f6f62b6ddb7861b4c40dcd93c
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-09-09 12:04:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (34.99%) - LeetSync

--- 35-search-insert-position/search-insert-position.c
+++ 35-search-insert-position/search-insert-position.c
@@ -1,30 +1,18 @@
 int searchInsert(int* nums, int numsSize, int target) {
-        for (int i = 0 ; i < numsSize ; i++){
-            if(nums[i]==target){
-                return i;
-            }
-        }
-        int q=numsSize-1;
-        while(q>=0){
-            if(target==0 && nums[0]>0){
-                return 0;
+    int left = 0,right = numsSize - 1;
+    int mid;
 
-            }
-            if(nums[q]<target){
-                return q+1;
-            }
-            else if(nums[0]>target){
-                return 0;
-            }
-            q--;
-            
+    while(left <= right){
+        mid = left + (right - left)/2;
+        if(nums[mid] == target){
+            return mid;
         }
-        
-        if (numsSize == 1 && target>nums[0]){
-            return 1;
+        else if(nums[mid] < target){
+            left = mid + 1;
         }
-        else if (numsSize == 1 && target<nums[0]){
-            return 0;
+        else{
+            right = mid - 1;
         }
-        return -1;
-}
+    }
+        return left;
+    }
