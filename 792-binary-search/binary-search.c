a5e7a4050f4e01d07987abd2cbc0cc9a4e6ee526
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-09-10 11:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (8.45%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+    int mid;
+    while(left <= right){
+        mid = left + (right - left)/2;
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+            left = mid +1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
