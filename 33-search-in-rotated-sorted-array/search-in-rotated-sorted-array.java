52e95434369747fb89133c554ecdf989018e9264
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-09-10 14:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (82.30%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n -1;
+        int mid;
+        while(left <= right){
+            mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && nums[mid] > target){
+                right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(nums[right] >= target && nums[mid] < target ){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+            }
+        }
+        return -1;
+    }
+}
