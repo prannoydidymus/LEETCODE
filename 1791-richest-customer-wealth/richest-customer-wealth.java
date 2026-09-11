e21bf76fc9b14542533533db8276847791646e83
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-09-11 13:48:34 +0530

    Time: 1 ms (15.05%) | Memory: 44.6 MB (55.73%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-       int maxacc = 0;
+        int maxacc = 0;
         int n = accounts.length;
-
-        for(int i = 0;i<n;i++)  {
+        for(int i = 0;i<n;i++){
             int current = 0;
-            for(int j = 0;j<accounts[i].length;j++){
-            current += accounts[i][j];
+            for(int j = 0;j< accounts[i].length;j++){
+                current += accounts[i][j];
+                            maxacc = Math.max(maxacc,current);
             }
-             maxacc = Math.max(maxacc,current);    
-        }    
-                return maxacc;
-        }
 
+        }
+        return maxacc;
+    }
 }
