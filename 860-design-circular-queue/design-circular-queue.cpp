4a5c606b66916857357cbd92381b36c2d1e6ce98
J.PRANNOY DIDYMUS <prannoydidymus@gmail.com>
2026-09-17 18:08:09 +0530

    Time: 11 ms (5.12%) | Memory: 23.6 MB (46.37%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.cpp
@@ -0,0 +1,61 @@
+class MyCircularQueue {
+    private:
+    std::vector <int> queue;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+public:
+    MyCircularQueue(int k) {
+       capacity = k;
+       queue.resize(k);
+
+    front = 0;
+    rear = -1;
+    size = 0;
+
+    }
+    
+    bool enQueue(int value) {
+        if(isFull()){
+            return false;
+        }
+            rear = (rear + 1) % capacity;
+            queue[rear] = value;
+            size++;  
+            return true;      
+    }
+    
+    bool deQueue() {
+        if(isEmpty()){
+            return false;
+        }
+front  = (front +1)% capacity;
+size--;
+return true;
+
+    }
+    
+    int Front() {
+        if(isEmpty()){
+            return -1;
+        }
+        return queue[front];
+    }
+    
+    int Rear() {
+       if(isEmpty()){
+        return -1;
+       } 
+       return queue[rear];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+};
